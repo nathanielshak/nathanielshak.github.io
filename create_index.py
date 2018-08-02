8b6e5b790a5b81d97b7ed20533b93776704db617
nathanielshak <nathaniel.shak@gmail.com>
2018-08-02 14:38:55 -0700

    changed organization

--- create_index.py
+++ create_index.py
@@ -7,8 +7,15 @@ message = """<html>
 """
 
 for filename in os.listdir('.'):
-	if os.path.isfile(filename):
-		message += "<a href=\"" + filename + "\">" + filename + "</a>"
+	if os.path.isfile(filename) and filename.endswith(".html"):
+		message += "<li><a href=\"" + filename + "\">" + filename + "</a></li>"
 
+for foldername in os.listdir('.'):
+	if os.path.isdir(foldername) and not foldername.endswith(".git"):
+		message += "<h2>" + foldername + "</h2>"
+		for filename in os.listdir("./" + foldername):
+			message += "<li><a href=\"" + foldername + "/" + filename + "\">" + filename + "</a></li>"
+
+message += "</html>"
 f.write(message)
 f.close()
